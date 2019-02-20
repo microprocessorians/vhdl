0cb67ab37b3513039903d4d9e8cbf7c1f7b36e79
ahmedashour2230 <45271677+ahmedashour2230@users.noreply.github.com>
2019-02-20 20:27:04 +0200

    Update data_mem.vhd

--- data_mem.vhd
+++ data_mem.vhd
@@ -22,43 +22,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_arith.ALL;
 use IEEE.STD_LOGIC_unsigned.ALL;
 entity data_mem is
-generic( data_width: integer:=8;
-          add_width: integer :=8);
+generic( data_width: integer:=32;
+          add_width: integer :=5);
     Port ( address : in  STD_LOGIC_VECTOR (add_width-1 downto 0);
-           data : inout  STD_LOGIC_VECTOR (data_width-1 downto 0);
-           cs : in  STD_LOGIC;
-           oe : in  STD_LOGIC;
-           we : in  STD_LOGIC;
+           data_in : in  STD_LOGIC_VECTOR (data_width-1 downto 0);
+			  data_out : out STD_LOGIC_VECTOR(data_width-1 downto 0);
+           RW : in  STD_LOGIC; 
            clck : in  STD_LOGIC);
 end data_mem;
 
 architecture Behavioral of data_mem is
 type ram is array (integer range<>) of std_logic_vector(data_width-1 downto 0);
-constant ram_depth: integer := 2** add_width;
-signal mem : ram (ram_depth-1 downto 0);
-signal data_out :STD_LOGIC_VECTOR (data_width-1 downto 0);
+signal mem : ram (0 to 2**add_width-1 );
 begin
---- write data  we=1, cs=1
+--- write data  RW=0
 Write_data:process(clck)
 begin
 if (rising_edge(clck)) then
-   if (we='1' and cs='1') then
-	  mem(conv_integer(unsigned(address)))<= data;
+   if ( RW='0') then
+	  mem(conv_integer(unsigned(address)))<= data_in;
 	 end if;
 end if;
 end process;
-
---- read data oe=1 , cs=1
-Read_data:process(clck)
-begin
-if (rising_edge(clck)) then
-   if ( cs='1' and oe='1') then
-	  data_out<=mem(conv_integer(unsigned(address)));
-	  
-	 end if;
-end if;
-end process;
-data<= data_out when ( cs='1' and oe='1') else
+--Read Data  RW=1
+data_out<= mem(conv_integer(unsigned(address))) when (RW='1' ) else
        (others=>'Z');
 end Behavioral;
 
